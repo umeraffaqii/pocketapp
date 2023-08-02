27eb4b5e0d687383eeb972c0c295823187546edd
ahmadziaamin <ahmadziaamin8@gmail.com>
2023-08-02 16:41:52 +0500

    scrool

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Home from './Component/Container/Home/Home';
 import Footer from './Component/Menu/Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import About from './Component/Container/About/About';
-import '../src/App.css'
+import '../src/App.css';
+import ScrollToTop from './Component/Menu/ScrollToTop';
 
 
 function App() {
   return (
     <Router>
       <Navbar />
+    <ScrollToTop/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
